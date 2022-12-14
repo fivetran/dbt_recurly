7514926865627d7e7d2f6e758ac05ef728e97057
Avinash Kunnath <avinash.kunnath@fivetran.com>
2022-12-14 12:20:16 -0800

    Remove extraneous CTE, materialize intermediate models as tables

--- models/recurly__account_daily_overview.sql
+++ models/recurly__account_daily_overview.sql
@@ -1,14 +1,8 @@
- with account_running_totals as (
+ with final as (
 
     select * 
     from {{ ref('int_recurly__account_running_totals') }}
-),
- 
-final as (
-
-    select *
-    from account_running_totals
-)    
+) 
 
 select *
 from final
